a6500d8e573d5d113ee49ff8978a35a78b2af750
Nathan Sasto <nsasto@gmail.com>
2025-04-10 16:20:04 +0100

    project structure cleanup for package distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='markitdown-langchain',
-    version='0.1.0',
+    name='langchain-markitdown',
+    version='0.1.2',
     description='LangChain data loaders based on Markdown by @untrueaxioms.',
     author='Nathan Sasto',
-     packages=find_packages(where='src'),
+    url="https://github.com/nsasto/langchain-markitdown",
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
         'markitdown[all]',  # Consider specifying a version
@@ -18,8 +23,27 @@ setup(
             'pytest',
             'python-docx',
             'python-pptx',
-            'openpyxl'
+            'openpyxl',
+            'pytest-cov'
+
         ]
     },
-    python_requires='>=3.8'  # Specify minimum Python version
+    python_requires='>=3.8',  
+    classifiers=[
+        'Development Status :: 3 - Alpha',  
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: Markdown',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',  # Adding the SPDX license identifier
 )
