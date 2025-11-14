89cb986621ce3a471731e8cafa74034494405d11
Nathan Sasto <nsasto@gmail.com>
2025-11-14 17:41:11 +0000

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name="langchain-markitdown",
-    version="0.1.8",
+    version="1.0.0",
     description="LangChain data loaders based on Markdown by @untrueaxioms.",
     author="Nathan Sasto",
     url="https://github.com/nsasto/langchain-markitdown",
